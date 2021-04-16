56035212a0db0913ad2443b2d803ffbb2f8a75f0
U-KOLIBRI\eugko <eugenekozlakov@gmail.com>
2021-04-15 23:41:04 -0400

    Added a few comments explaining things, as well as my thoughts.

--- test1_ekozlakov.m
+++ test1_ekozlakov.m
@@ -1,3 +1,6 @@
-IMG=readjpg('tux.jpg');
-modIMG = colorFinder(IMG,0,0,0,255,0,0);
-dispimg(modIMG);
+IMG=readjpg('tux.jpg'); %copy of final.m, just compressed colorFinder function into another file
+modIMG = colorFinder(IMG,0,0,0,255,0,0); %saves results of colofinder to modIMG
+dispimg(modIMG); %display modIMG modified image
+
+%i dont think final.m works for the reasons originally thought. It gets it
+%dont right, but how it gets it done is wrong.
