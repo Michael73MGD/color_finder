774b8eb03e0d59697d0c4f953c39b9e762d19080
Michael Dasaro <michaelgdasaro@gmail.com>
2021-05-05 14:23:10 -0400

    idk

--- test1_ekozlakov.m
+++ test1_ekozlakov.m
@@ -1,5 +1,5 @@
 IMG=readjpg('tux.jpg'); %copy of final.m, just compressed colorFinder function into another file
-modIMG = colorFinder(IMG,0,0,0,255,0,0); %saves results of colofinder to modIMG
+modIMG = colorFinder(IMG,200,255,0,100,0,100); %saves results of colofinder to modIMG
 dispimg(modIMG); %display modIMG modified image
 
 %i dont think final.m works for the reasons originally thought. It gets it
