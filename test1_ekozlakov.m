706aa729ba9609f9d2fb91ba3d37bf3c47e620da
Gene K <ekozlako@stevens.edu>
2021-05-09 17:04:28 -0400

    Remove old files

--- test1_ekozlakov.m
+++
@@ -1,6 +0,0 @@
-IMG=readjpg('tux.jpg'); %copy of final.m, just compressed colorFinder function into another file
-modIMG = colorFinder(IMG,190,250,225,255,225,255); %saves results of colofinder to modIMG
-%dispimg(modIMG); %display modIMG modified image
-
-%i dont think final.m works for the reasons originally thought. It gets it
-%dont right, but how it gets it done is wrong.
