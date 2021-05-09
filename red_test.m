832fc8664d2978db0333fe4b0c25583cb4daee5c
U-KOLIBRI\eugko <eugenekozlakov@gmail.com>
2021-05-09 15:39:11 -0400

    add filesaves, results folder

--- red_test.m
+++ red_test.m
@@ -1,2 +1,6 @@
 IMG=readjpg('tux');
 [modIMG, grayscale] = colorFinder(IMG,200,255,0,100,0,100); 
+%imsave(modIMG);    %nonfunctional
+%imsave(grayscale); %nonfunctional
+imwrite(modIMG, 'results/modIMG.jpg');
+imwrite(grayscale, 'results/grayscale.jpg');
